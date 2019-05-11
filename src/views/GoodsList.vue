7a087bb6a0d4ba46abc62424a15d12c60b85545a
cjshinning <784319417@qq.com>
2019-05-11 12:51:47 +0800

    vue router 动态路由

---
+++ src/views/GoodsList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>这是商品列表页</h2>
+        <span>{{$route.params.id}}</span><br/>
+        <span>{{$route.params.name}}</span>
+    </div>
+    
+</template>
+<script>
+export default {
+    
+}
+</script>
