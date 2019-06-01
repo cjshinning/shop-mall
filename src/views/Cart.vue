d3e8d280f5ce8dc58b539e971a7d7172ad5f20f6
cjshinning <784319417@qq.com>
2019-06-01 23:03:01 +0800

    订单确认列表渲染

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -75,7 +75,7 @@
                                     </div>
                                 </div>
                                 <div class="cart-tab-2">
-                                    <div class="item-price">{{item.salePrice}}</div>
+                                    <div class="item-price">{{item.salePrice | currency('$')}}</div>
                                 </div>
                                 <div class="cart-tab-3">
                                     <div class="item-quantity">
@@ -89,7 +89,7 @@
                                     </div>
                                 </div>
                                 <div class="cart-tab-4">
-                                    <div class="item-price-total">{{item.productNum*item.salePrice}}</div>
+                                    <div class="item-price-total">{{(item.productNum*item.salePrice) | currency('$')}}</div>
                                 </div>
                                 <div class="cart-tab-5">
                                     <div class="cart-item-opration">
