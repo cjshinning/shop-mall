9523827e3198d383ac6c24d7dbab2817bd6a6052
cjshinning <784319417@qq.com>
2019-06-01 20:37:03 +0800

    地址列表渲染

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -121,7 +121,7 @@
                                 Item total: <span class="total-price">{{totalPrice | currency('$')}}</span>
                             </div>
                             <div class="btn-wrap">
-                                <a class="btn btn--red">Checkout</a>
+                                <a class="btn btn--red" v-bind:class="{'btn--dis':checkedCount==0}" @click="checkOut">Checkout</a>
                             </div>
                         </div>
                     </div>
@@ -271,6 +271,13 @@ export default {
                     console.log('update success!')
                 }
             })
+        },
+        checkOut(){
+            if(this.checkedCount > 0){
+                this.$router.push({
+                    path: '/address'
+                })
+            }
         }
     },
     components: {
